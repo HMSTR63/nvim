34b07f09eaaec7b438fe12d031312020fba9a898
SOUFIANE JAMMALI <soufianejamali14@gmail.com>
2024-06-10 16:16:25 +0100

    yarbi tsda9 2

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,7 +7,8 @@ return{
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', {})
+    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+    vim.keymap.set('n', '<C-b>', ':Neotree toggle<CR>', {})
   end
 }
 
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,7 +1,9 @@
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
 vim.g.mapleader = " "
-
+vim.g.user = "sojammali"
+vim.g.mail = "sojammali1337@gmail.com"
+vim.opt.clipboard:append("unnamedplus")
